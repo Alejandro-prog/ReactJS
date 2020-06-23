b3aa04f38cc45207a23d2ce533d87cbe80e169a7
ale <alejandro>
2020-06-23 18:20:27 -0500

    Location, WeatherData, WeatherLocation

--- wheather-app/src/App.js
+++ wheather-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import WeatherLocation from './components/WeatherLocation';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Wheather app
+      <WeatherLocation></WeatherLocation>
     </div>
   );
 }
